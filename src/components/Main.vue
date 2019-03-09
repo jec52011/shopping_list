3d8afcf756032a289c27a5b662c1e156abdd8419
Jed Chao <chaoj1@southernct.edu>
2019-03-09 02:20:33 -0500

    Added working text inputs and button to add item to list

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,54 +1,80 @@
 <template>
-  <div>
+  <b-container fluid>
     <h1>Shopping List</h1>
-    <b-table striped hover :items="items" :fields="fields">
-      <template slot="strike" slot-scope="data">
-        <b-form-checkbox v-model="data.item.strike" :checked="changeVariant(data.item)">{{data.value}}</b-form-checkbox>
-      </template>
+    <b-row>
+      <b-table striped hover :items="items" :fields="fields">
+        <template slot="strike" slot-scope="data">
+          <b-form-checkbox v-model="data.item.strike" :checked="changeVariant(data.item)">{{data.value}}</b-form-checkbox>
+        </template>
 
-      <template slot="item" slot-scope="data">
-        {{ data.value }}
-      </template>
+        <template slot="item" slot-scope="data">
+          {{ data.value }}
+        </template>
 
-      <template slot="store" slot-scope="data">
-        {{ data.value }}
-      </template>
+        <template slot="store" slot-scope="data">
+          {{ data.value }}
+        </template>
 
-      <template slot="location" slot-scope="data">
-        {{ data.value }}
-      </template>
+        <template slot="location" slot-scope="data">
+          {{ data.value }}
+        </template>
 
-    </b-table>
+      </b-table>
+    </b-row>
 
-    <b-form inline @submit="onSubmit">
-      <label class="sr-only" for="inputName">Item: </label>
-      <b-input
-        class="mb-2 mr-sm-2 mb-sm-0"
-        id="inputName"
-        placeholder="Item name"
-        type="text"
-        required />
-      <b-button type="submit" variant="primary">Submit</b-button>
-    </b-form>
+    <b-row>
+      <b-form class="mx-auto" inline @submit="addItem">
+        <label class="sr-only" for="inputName">Item: </label>
+        <b-input
+          class="mb-2 mr-sm-2 mb-sm-0"
+          id="inputName"
+          placeholder="Item name"
+          type="text"
+          v-model="form.newItem"
+          required />
 
-    <b-button @click="addItem">Add</b-button>
-  </div>
+        <label class="sr-only" for="inputStore">Store: </label>
+        <b-input
+          class="mb-2 mr-sm-2 mb-sm-0"
+          id="inputStore"
+          placeholder="Store"
+          type="text"
+          v-model="form.newStore"
+           />
+
+        <label class="sr-only" for="inputLocation">Location: </label>
+        <b-input
+          class="mb-2 mr-sm-2 mb-sm-0"
+          id="inputName"
+          placeholder="Location"
+          type="text"
+          v-model="form.newLocation"
+           />
+        <b-button type="submit" variant="primary">Add</b-button>
+      </b-form>
+    </b-row>
+
+
+  </b-container>
 </template>
 
 <script>
-// 1. CSS to strikeout a whole row
-// 2. Approach #1 using _rowVariant, which we made conditional based on checkbox
-// 3. Override how table-danger behaves
 // Next time, text fields for adding, filter?, add location data
 export default {
   data () {
     return {
-      items: [
-        { strike: true, item: 'Quinoa', store: 'Costco', location: 'Norwalk', _rowVariant: 'danger' },
-        { strike: false, item: 'Stand Mixer', store: 'Macy\'s', location: 'Trumbull', _rowVariant: 'danger' },
-        { strike: true, item: 'Polo Shirt', store: 'Macy\'s', location: 'Trumbull', _rowVariant: 'danger' },
-        { strike: false, item: 'Carrots', store: 'Costco', location: 'Any', _rowVariant: 'danger' }
-      ],
+      form: {
+        newItem:'',
+        newStore:'',
+        newLocation:''
+      },
+      items: [],
+      // items: [
+      //   { strike: true, item: 'Quinoa', store: 'Costco', location: 'Norwalk', _rowVariant: 'danger' },
+      //   { strike: false, item: 'Stand Mixer', store: 'Macy\'s', location: 'Trumbull', _rowVariant: 'danger' },
+      //   { strike: true, item: 'Polo Shirt', store: 'Macy\'s', location: 'Trumbull', _rowVariant: 'danger' },
+      //   { strike: false, item: 'Carrots', store: 'Costco', location: 'Any', _rowVariant: 'danger' }
+      // ],
       fields: [
         { key: 'strike', label: 'Check' },
         { key: 'item', label: 'Item' },
@@ -59,7 +85,10 @@ export default {
   },
   methods: {
     addItem () {
-      this.items.push({strike: false, item: 'Jeans', store: 'Kohl\'s', location: 'Any'})
+      this.items.push({strike: false, item: this.form.newItem, store: this.form.newStore, location: this.form.newLocation})
+      this.form.newItem = ''
+      this.form.newStore = ''
+      this.form.newLocation = ''
     },
     changeVariant (row) {
       if (row.strike == true) {
@@ -71,7 +100,11 @@ export default {
     },
     onSubmit (evt) {
       evt.preventDefault()
-      alert(JSON.stringify(this.form))
+      //alert(JSON.stringify(this.form.name))
+      this.items.push({strike: false, item: this.form.newItem, store: this.form.newStore, location: this.form.newLocation})
+      this.form.newItem = ''
+      this.form.newStore = ''
+      this.form.newLocation = ''
     }
   }
 }
@@ -92,7 +125,10 @@ export default {
     background-color: transparent;
   }
   .table-hover .table-danger:hover > td, .table-hover .table-danger:hover > th {
-    background-color: transparent;
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+  .table-hover .table-danger:hover {
+  	background-color: rgba(0, 0, 0, 0.05);
   }
   .table-danger th, .table-danger td, .table-danger thead th, .table-danger tbody + tbody {
     border-color: rgb(222, 226, 230);
